3d4349dcee2d9a45c5e17c6d94402db97472c1b1
EmanAmir13 <eman.amir@flicken.io>
2025-03-06 15:22:49 +0500

    Class base middleware

--- blog/middleware.py
+++ blog/middleware.py
@@ -1,12 +1,24 @@
 # Function base middlewares
+# def my_middleware(get_response):
+#     print('One time Initialization')
+#
+#     def middleware(request):
+#         print('This is before view')
+#         response = get_response(request)
+#         print('This is after view')
+#         return response
+#
+#     return middleware
 
-def my_middleware(get_response):
-    print('One time Initialization')
 
-    def middleware(request):
+# Class base middleware
+class MyMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        print('One time Initialization')
+
+    def __call__(self, request):
         print('This is before view')
-        response = get_response(request)
+        response = self.get_response(request)
         print('This is after view')
         return response
-
-    return middleware
